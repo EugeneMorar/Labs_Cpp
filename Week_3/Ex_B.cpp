8053676678a190f34dec3c8641c9b975598df1a9
EugeneMoraru <melnikov.ev@phystech.edu>
2021-02-27 21:08:30 +0300

    B done.

--- Week_3/Ex_B.cpp
+++ Week_3/Ex_B.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 #include <vector>
 #include <tuple>
 
@@ -7,41 +6,54 @@
 using namespace std;
 
 int main() {
-    int N, M, K, a, b, status;
+    int N, M, K, a, b;
     cin >> N;
     cin >> M;
     cin >> K;
 
-    vector <tuple < int, int > > coord_list;
+    vector <tuple<int, int>> coord_list;
+    coord_list.reserve(K);
 
-    vector <vector <int> > coord;
-    coord.reserve(2 * N);
+    vector <vector <int>> field;
+    field.reserve(N);
 
-    vector <int> coord_row(100, 0);
+    vector <int> field_row(M, 0);
 
-    vector <vector <int> > field;
-    field.reserve(2 * N);
 
-    vector <int> field_row;
-    field_row.reserve(100);
+    for (int i = 0; i < N; i++) {
+        field.push_back(field_row);
+    }
 
     for (int i = 0; i < K; i++) {
         cin >> a;
         cin >> b;
-
-        tuple <int, int> coord;
-        coord = make_tuple(a, b);
-        coord_list.push_back(coord);
+        coord_list.push_back(make_tuple(a-1, b-1));
     }
 
     for (int i = 0; i < N; i++) {
-        coord_row = coord[i];
-        for (int k = 0; k < M; k++) {
-            if
+        for (int j = 0; j < M; j++) {
+            for (auto it : coord_list) {
+                if ((((get<0>(it) >= i-1) and (get<0>(it) <= i+1))
+                and ((get<1>(it) >= j-1) and (get<1>(it) <= j+1)))
+                and (get<0>(it) != i and get<1>(it) != j))
+                    (field[i])[j] += 1;
+                if ((get<0>(it) == i and ((get<1>(it) >= j-1) and (get<1>(it) <= j+1)))
+                    and get<1>(it) != j)
+                    (field[i])[j] += 1;
+                if ((get<1>(it) == j and (get<0>(it) >= i-1) and (get<0>(it) <= i+1)
+                and (get<0>(it) != i)))
+                    (field[i])[j] += 1;
+                }
+            }
         }
+
+    for (auto it : coord_list)
+        (field[get<0>(it)])[get<1>(it)] = -1;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++)
+            cout << (field[i])[j] << " ";
         cout << '\n';
     }
-
 
     return 0 ;
 }
