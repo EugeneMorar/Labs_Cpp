1604cdeffbca1f94f4e049be5c2d62c3825eec6d
EugeneMoraru <melnikov.ev@phystech.edu>
2021-03-01 10:50:13 +0300

    Week_5 Ex_A done.

---
+++ Week_5/Ex_A.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <sstream>
+#include <stack>
+
+
+using namespace std;
+
+int main() {
+    int iter = 0, top_element;
+    string data;
+    stack<int> stacc;
+    vector<int> modified_data;
+
+    getline(cin, data);
+    istringstream iss(data);
+
+    copy(istream_iterator<int>(iss), istream_iterator<int>(), inserter(modified_data,
+                          modified_data.end()));
+
+    while (modified_data[iter] != 0) {
+        if (modified_data[iter] > 0)
+            stacc.push(modified_data[iter]);
+        if (modified_data[iter] < 0 and abs(modified_data[iter]) < stacc.top() and not stacc.empty()) {
+            top_element = modified_data[iter] + stacc.top();
+            stacc.pop();
+            stacc.push(top_element); }
+        if (modified_data[iter] < 0 and abs(modified_data[iter]) <= stacc.top() and not stacc.empty()) {
+            stacc.pop();
+        }
+        iter++;
+    }
+    if (stacc.empty())
+        cout << -1 << endl;
+    else
+        cout << stacc.size() << " " << stacc.top() << endl;
+
+    return 0;
+}
