4be9bf07009847ddd914bf204fe798de9154c50a
EugeneMoraru <melnikov.ev@phystech.edu>
2021-03-03 17:26:31 +0300

    Done X_Brackets of week 5.

--- Week_5/Ex_X_Brackets.cpp
+++ Week_5/Ex_X_Brackets.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <string>
-#include <stack>
+#include <vector>
 
 using namespace std;
 
 int main() {
     string line;
-    stack<char> bra, ket;
+    vector<char> bra, ket;
 
     cin >> line;
     for (char & i : line) {
         if (i == ')' or i == ']' or i == '}' or i == '>')
-            bra.push(i);
+            bra.push_back(i);
         else
-            ket.push(i);
+            ket.push_back(i);
     }
-    if (bra.size() == ket.size())
+    if (bra.size() == ket.size() and line[0] == bra[0]
+    and line[line.size() - 1] == ket[ket.size() - 1])
         cout << "YES";
     else
         cout << "NO";
