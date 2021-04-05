4e1aff507929b74693fc6d892b4420ecea59e21e
EugeneMoraru <melnikov.ev@phystech.edu>
2021-04-05 12:05:25 +0300

    Ex_X_Brackets_star fixed.

--- Week_5/Ex_X_Brackets_star.cpp
+++ Week_5/Ex_X_Brackets_star.cpp
@@ -1,26 +1,43 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <sstream>
 
 using namespace std;
 
 int main() {
-    string line;
+    int N;
+    string line, in, tmp;
+    vector<string> brackets;
     vector<char> bra, ket;
 
+    cin >> N;
+    cin.clear();
+    cin.ignore(1, '\n');
+
+
+    getline(cin, in);
+    istringstream iss(in);
+    while (iss >> tmp)
+        brackets.push_back(tmp);
+
+
     cin >> line;
+
     for (char & i : line) {
-        if (find(line.begin(), line.end(), char(i + 1)) != line.end())
-            bra.push_back(i);
-        else if (find(line.begin(), line.end(), char(i - 1)) != line.end())
-            ket.push_back(i);
+        for (int k = 0; k < brackets.size(); k++) {
+            if (i == brackets[k][0])
+                bra.push_back(i);
+            else if (i == brackets[k][1])
+                ket.push_back(i);
+        }
     }
-    if (bra.size() + ket.size() == line.size() and line[0] == bra[0]
+    if (bra.size() == ket.size() and line[0] == bra[0]
         and line[line.size() - 1] == ket[ket.size() - 1])
         cout << "YES";
     else
         cout << "NO";
+
 
     return 0;
 }
